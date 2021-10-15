b25f9207f91e6302d3b2cd25e4db8f488a4b9a52
Lucas Peres <lucasperesss@gmail.com>
2021-10-14 23:57:51 -0300

    pokedex com filtro por tipo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
 import './App.css';
-import pokemons from './data';
 import Pokedex from './Pokedex';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1 className='title'>Sua Pokedex</h1>
+        <Pokedex />
+      </div>
+    );
+  }
 }
 
 export default App;
